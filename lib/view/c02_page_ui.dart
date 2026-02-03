92436d56dc85d0b281c944502addddf987ded894
Thiti1012 <s6619410031@sau.ac.th>
2026-02-03 20:33:03 +0700

    commit07: ทำc2เสร็จแล้ว

--- lib/view/c02_page_ui.dart
+++ lib/view/c02_page_ui.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/view/c03_page_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class C02PageUi extends StatefulWidget {
   const C02PageUi({super.key});
@@ -10,6 +13,257 @@ class C02PageUi extends StatefulWidget {
 class _C02PageUiState extends State<C02PageUi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 50.0, right: 50.0),
+              child: Row(
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.075),
+                  Text(
+                    "9:19",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Icon(Icons.signal_cellular_alt, size: 18),
+                  SizedBox(width: 5),
+                  Icon(Icons.wifi, size: 18),
+                  SizedBox(width: 5),
+                  Icon(FontAwesomeIcons.batteryEmpty, size: 18),
+                ],
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.075),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.2,
+              height: MediaQuery.of(context).size.width * 0.2,
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: Color(0xFF34A751),
+                  width: 3.0,
+                ),
+              ),
+              child: Image.asset(
+                'assets/images/img10.png',
+                fit: BoxFit.contain,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 50.0, right: 50.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                  Text(
+                    'Sign in your account',
+                    style: GoogleFonts.poppins(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF000000),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                  Text(
+                    'Email',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF000000),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 10.0),
+                      filled: true,
+                      fillColor: Color(0xFFFAFAFA),
+                      hintText: 'ex:jon.smith@gmail.com',
+                      hintStyle: GoogleFonts.poppins(
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        color: Color(0xFF888888),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                  Text(
+                    'Password',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF000000),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 10.0),
+                      filled: true,
+                      fillColor: Color(0xFFFAFAFA),
+                      hintText: '*********',
+                      hintStyle: GoogleFonts.poppins(
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        color: Color(0xFF888888),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.06),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Sign in',
+                        style: GoogleFonts.poppins(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFFFFFFFF),
+                        )),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(
+                        MediaQuery.of(context).size.width * 0.85,
+                        MediaQuery.of(context).size.height * 0.06,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      backgroundColor: Color(0xFF00B140),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      textStyle: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                  Center(
+                    child: Text(
+                      'Or sign in with',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFF888888),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Image.asset(
+                            'assets/icon/google.png',
+                            width: MediaQuery.of(context).size.width * 0.1,
+                            height: MediaQuery.of(context).size.height * 0.1,
+                            fit: BoxFit.contain,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size(
+                              MediaQuery.of(context).size.width * 0.20,
+                              MediaQuery.of(context).size.height * 0.06,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            backgroundColor: Color(0xFFF4F4F4),
+                          ),
+                        ),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.05),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Image.asset(
+                            'assets/icon/facebook1.png',
+                            width: MediaQuery.of(context).size.width * 0.1,
+                            height: MediaQuery.of(context).size.height * 0.1,
+                            fit: BoxFit.contain,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size(
+                              MediaQuery.of(context).size.width * 0.20,
+                              MediaQuery.of(context).size.height * 0.06,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            backgroundColor: Color(0xFFF4F4F4),
+                          ),
+                        ),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.05),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Image.asset(
+                            'assets/icon/twitter.png',
+                            width: MediaQuery.of(context).size.width * 0.1,
+                            height: MediaQuery.of(context).size.height * 0.1,
+                            fit: BoxFit.contain,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size(
+                              MediaQuery.of(context).size.width * 0.20,
+                              MediaQuery.of(context).size.height * 0.06,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            backgroundColor: Color(0xFFF4F4F4),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Don\'t have an account? ',
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF888888),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => C03PageUi()));
+                        },
+                        child: Text(
+                          'Sign up',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xFF00B140),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
