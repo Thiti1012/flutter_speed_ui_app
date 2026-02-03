341909f6b81af285c5e31e814749ae9791328db5
Thiti1012 <s6619410031@sau.ac.th>
2026-02-03 15:42:52 +0700

    commit06: ทำc1เสร็จเเล้ว

--- lib/view/c01_page_ui.dart
+++ lib/view/c01_page_ui.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/view/c02_page_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class C01PageUi extends StatefulWidget {
   const C01PageUi({super.key});
@@ -9,9 +11,67 @@ class C01PageUi extends StatefulWidget {
 
 class _C01PageUiState extends State<C01PageUi> {
   @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => C02PageUi(),
+        ),
+      );
+    });
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 0, 0),
+      body: Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/img8.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 150),
+                Image.asset(
+                  'assets/images/img10.png',
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  height: MediaQuery.of(context).size.width * 0.25,
+                  fit: BoxFit.contain,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'HOPE FOR\nHUMANITY',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 1.5,
+                  ),
+                ),
+                SizedBox(height: 200),
+                Text(
+                  'Welcome to\nhope for humanity',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF005010),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
